225d8ed9d268dadb30c99e237806ba02f5792793
darwin <oleg.umarov@gmail.com>
2016-03-14 14:28:42 -0500

    started on linked list

---
+++ LinkedList.js
@@ -0,0 +1,32 @@
+//NODE value inside list
+//function takes a value 
+//set value to value
+//set next to null
+
+//LINKED LIST
+//functions takes a value
+//set head to the node with a value
+//set tail to the head
+
+
+// add to tail
+// function takes a value
+// create a new node with a value
+// tail.next = tail;
+// tail = new Node with value
+
+
+// remove head
+
+
+//contains
+
+
+var linkedList = new LinkedList(5);
+linkedlist.addtoTail(5);
+linkedLIst.removeHead() => head
+linkedList.contains(5) => true or false
+
+var linkedList = {head: Node, tail: Node}
+Node = {value: 4, next: Node}
+
